cd9f2ba557aed45ca4a8de6025eec24831474b89
Nory Lazo <nlazo@me.com>
2022-08-12 12:48:17 -0600

    Completed all advanced hook examples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import UseMemoExample from './components/UseMemoExample';
+import CustomHookExample2 from './components/CustomHookExample2';
 
 function App() {
   return (
     <div className='container mt-5'>
-      <UseMemoExample />
+      <CustomHookExample2 />
     </div>
   );
 }
